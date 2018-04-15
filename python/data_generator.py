9bcc5a8ff9c4b8db45d1f88df28e8c59d92406fc
Jean Metz <jmetzz@gmail.com>
2018-04-15 13:50:49 +0200

    add iris dataset (https://archive.ics.uci.edu/ml/datasets/iris)

---
+++ python/data_generator.py
@@ -0,0 +1,11 @@
+# %matplotlib inline
+import matplotlib.pyplot as plt
+import seaborn as sns; sns.set()  # for plot styling
+import numpy as np
+from sklearn.datasets.samples_generator import make_blobs
+
+X, y_true = make_blobs(n_samples=300, centers=4,
+                       cluster_std=0.60, random_state=0)
+plt.scatter(X[:, 0], X[:, 1], s=50);
+
+
