c4bf098d5560146d0d2c22fd5b27b3188b43c336
Jean Metz <jmetzz@gmail.com>
2018-04-22 20:41:41 +0200

    add evaluation metric signatures

--- python/evaluation.py
+++ python/evaluation.py
@@ -5,3 +5,24 @@ def accuracy(test_set, predictions):
             correct += 1
     return (correct / float(len(test_set))) * 100.0
 
+
+def precision(gold_truth, predictions):
+    # precision = tp / tp_fp
+    raise NotImplementedError
+
+
+def recall(gold_truth, predictions):
+    # recall = tp / tp_fn
+    raise NotImplementedError
+
+
+def f1(gold_truth, predictions):
+    raise NotImplementedError
+
+
+def confusion_matrix(gold_truth, predictions):
+    raise NotImplementedError
+
+
+def basic_evaluation(gold_truth, prediction):
+    return precision(gold_truth, prediction), recall(gold_truth, prediction)
