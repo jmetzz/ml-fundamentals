3806d23183b241b5cde098e03d76104558289234
Jean Metz <jmetzz@gmail.com>
2018-04-23 19:23:14 +0200

    Add more activation functions to be used with neural networks

--- python/rna.py
+++ python/rna.py
@@ -1,33 +1,49 @@
+import math
+
+
+def step(x):
+    return 1.0 if x >= 0.0 else 0.0
+
+
+def sigmoid(x):
+    return 1 / (1 + math.exp(-x))
+
+def hypertang(x):
+    exp = math.exp(2 * x)
+    return (exp - 1) / (exp + 1)
+
 class Perceptron:
 
-    def __init__(self, bias, dimension):
-        self.bias = bias
-        self.dimension = dimension
+    def __init__(self, activation_function=step):
+        self.activation = activation_function
 
     def predict(self, x, w):
-        return 1.0 if self._activation(x, w) >= 0.0 else 0.0
+        s = self._summation(x, w)
+        return self.activation(s)
 
-    def _activation(self, x, w):
+    def _summation(self, x, w):
         # The first weight is always the bias as it is standalone
         # and not responsible for a specific input value.
         activation = w[0]
-        for i in range(self.dimension):
+        for i in range(len(w) - 1):
             activation += w[i + 1] * x[i]
         return activation
 
 
 class SingleLayerPerceptron:
-    def __init__(self, bias, dimension, epochs=50, learning_rate=0.001):
-        self.neuron = Perceptron(bias, dimension)
+    def __init__(self, bias, dimension, epochs=50, learning_rate=0.001, activation_function=step):
+        self.neuron = Perceptron(activation_function)
+        self.bias = bias
         self.dimension = dimension
         self.eta = learning_rate
         self.epochs = epochs
 
     def train(self, data):
-        weights = [0.0 for i in range(self.dimension + 1)]
+        weights = [0.0] * (self.dimension + 1)
         for e in range(self.epochs):
             sum_error = 0.0
             for x in data:
+                # print(weights) # uncomment the check the change in weights
                 predicted = self.neuron.predict(x[:-1], weights)
                 error = x[-1] - predicted
                 sum_error += (error) ** 2
@@ -51,14 +67,15 @@ dataset = [[2.7810836, 2.550537003, 0],
 
 
 def main():
-    p = Perceptron(0.5, 2)
+    p = Perceptron()
+    # The values of bias and weights are typically set randomly and then updated using gradient descent
     w = [-0.1, 0.20653640140000007, -0.23418117710000003]
     for row in dataset:
         prediction = p.predict(row, w)
         print("Expected=%d, Predicted=%d" % (row[-1], prediction))
 
     print("-----------------------------")
-    rna = SingleLayerPerceptron(-0.1, 2, 10, 0.1)
+    rna = SingleLayerPerceptron(-0.1, 2, 50, 0.01, sigmoid)
 
     nweights = rna.train(dataset)
     print("Network weights: ")
